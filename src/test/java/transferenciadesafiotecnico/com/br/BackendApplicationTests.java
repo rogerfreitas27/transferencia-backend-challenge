30ac6d3804266069cb8451b6912fd9939907bd80
Roger Freitas <roger.freitas@supera.com.br>
2026-03-20 19:06:22 -0300

    Initial commit

---
+++ src/test/java/transferenciadesafiotecnico/com/br/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package transferenciadesafiotecnico.com.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
